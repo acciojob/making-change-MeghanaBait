8744a85b4c43844f739d3f0f015c24f1272133c7
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-19 11:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const makeChange = (c) => {
   // your name here
+	const p = 1; 
+	const n = 5;
+	const d = 10;
+	const q = 25;
+
+
+	let change = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    };
+
+	change.q = Math.floor(c/q);
+	c -= change.q * q;
+
+	change.d = Math.floor(c/d);
+	c -= change.d * d;
+
+	change.n = Math.floor(c/n);
+	c -= change.n * n;
+
+	change.p = c;
+	
+	return change;
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
